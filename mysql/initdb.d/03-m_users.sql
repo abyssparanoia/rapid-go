ce257e639ddd5f9a76d1fcf9c0064df5e293d4d1
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-02-17 23:22:45 +0900

    feat: add sql for insert seed data

---
+++ mysql/initdb.d/03-m_users.sql
@@ -0,0 +1,9 @@
+INSERT INTO `rapid_go`.m_users (
+    id,name,avatar_path,sex,enabled,created_at,updated_at
+)
+VALUES
+    (1,'山田太郎','https://google.api.storage','man',true,'2019-02-06 20:55:52','2019-02-06 20:55:52')
+    (2,'佐藤二郎','https://google.api.storage','man',true,'2019-02-06 20:55:52','2019-02-06 20:55:52')
+    (3,'田中三郎','https://google.api.storage','man',true,'2019-02-06 20:55:52','2019-02-06 20:55:52')
+    (4,'林花子','https://google.api.storage','woman',true,'2019-02-06 20:55:52','2019-02-06 20:55:52')
+    (5,'村田涼子','https://google.api.storage','woman',true,'2019-02-06 20:55:52','2019-02-06 20:55:52')
