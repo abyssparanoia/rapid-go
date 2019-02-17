d10844c584b7170a049052aa439ad119aa746b6b
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-02-17 22:38:21 +0900

    feat: add user meta data

--- api/src/model/user.go
+++ api/src/model/user.go
@@ -6,11 +6,11 @@ import (
 
 // User ... ユーザーモデル
 type User struct {
-	ID         int64     ``
-	Name       string    ``
-	AvatarPath string    ``
-	Sex        string    ``
-	Enabled    bool      ``
-	CreatedAt  time.Time ``
-	UpdatedAt  time.Time ``
+	ID         int64     `json:"id"`
+	Name       string    `json:"name"`
+	AvatarPath string    `json:"avatar_path"`
+	Sex        string    `json:"sex"`
+	Enabled    bool      `json:"enabled"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
 }
