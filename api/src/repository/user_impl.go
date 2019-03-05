f2462b876f4d64401ec19ce2372a2e00a02af390
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-03-06 02:43:04 +0900

    feat: add insert repository

--- api/src/repository/user_impl.go
+++ api/src/repository/user_impl.go
@@ -4,8 +4,12 @@ import (
 	"context"
 	"database/sql"
 
+	"github.com/abyssparanoia/rapid-go/api/src/lib/log"
+	"github.com/abyssparanoia/rapid-go/api/src/lib/mysql"
 	"github.com/abyssparanoia/rapid-go/api/src/lib/util"
 	"github.com/abyssparanoia/rapid-go/api/src/model"
+
+	sq "github.com/Masterminds/squirrel"
 )
 
 type user struct {
@@ -24,6 +28,22 @@ func (r *user) Get(ctx context.Context, userID int64) (*model.User, error) {
 	}
 
 	return user, nil
+}
+
+func (r *user) Insert(ctx context.Context, src *model.User) error {
+	q := sq.Insert("m_users").
+		Columns("id", "name", "avatar_path", "sex", "enabled", "created_at", "updated_at").
+		Values(src.ID, src.Name, src.AvatarPath, src.Sex, src.Enabled, src.CreatedAt, src.UpdatedAt)
+
+	mysql.DumpInsertQuery(ctx, q)
+
+	_, err := q.RunWith(r.sql).ExecContext(ctx)
+	if err != nil {
+		log.Errorf(ctx, "MySQLInsert: %s", err.Error())
+		return err
+	}
+
+	return nil
 }
 
 // NewUser ... ユーザーレポジトリを取得する
