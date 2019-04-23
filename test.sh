891b12ff107fc046ea1225f83bad29c65b1e93f0
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-04-23 15:22:45 +0900

    fix: remove unused bash file

--- test.sh
+++
@@ -1,3 +0,0 @@
-for file in `\find ./src/service/*.go -maxdepth 1 -type f ! -name "*_impl.go" `; do
-    docker-compose exec api mockgen -source ${PROJECT_DIR}/src/service/${file}.go -destination ${PROJECT_DIR}/src/service/mock/${file}.go
-done
