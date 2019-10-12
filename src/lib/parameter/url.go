ae0244a1f2a261a1a2e7b0f091aeb3c192f028b8
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-10-12 15:29:14 +0900

    feat: impl form parameter method

--- src/lib/parameter/url.go
+++ src/lib/parameter/url.go
@@ -15,43 +15,43 @@ func GetURL(r *http.Request, key string) string {
 }
 
 // GetURLByInt ... get url parameter by int
-func GetURLByInt(ctx context.Context, r *http.Request, key string) (*int, error) {
+func GetURLByInt(ctx context.Context, r *http.Request, key string) (int, error) {
 	str := chi.URLParam(r, key)
 	if str == "" {
-		return nil, nil
+		return 0, nil
 	}
 	num, err := strconv.Atoi(str)
 	if err != nil {
 		log.Warningm(ctx, "strconv.Atoi", err)
-		return nil, err
+		return 0, err
 	}
-	return &num, nil
+	return num, nil
 }
 
 // GetURLByInt64 ... get url parameter by int64
-func GetURLByInt64(ctx context.Context, r *http.Request, key string) (*int64, error) {
+func GetURLByInt64(ctx context.Context, r *http.Request, key string) (int64, error) {
 	str := chi.URLParam(r, key)
 	if str == "" {
-		return nil, nil
+		return 0, nil
 	}
 	num, err := strconv.ParseInt(str, 10, 64)
 	if err != nil {
 		log.Warningm(ctx, "strconv.ParseInt", err)
-		return nil, err
+		return 0, err
 	}
-	return &num, nil
+	return num, nil
 }
 
 // GetURLByFloat64 ... get url parameter by float64
-func GetURLByFloat64(ctx context.Context, r *http.Request, key string) (*float64, error) {
+func GetURLByFloat64(ctx context.Context, r *http.Request, key string) (float64, error) {
 	str := chi.URLParam(r, key)
 	if str == "" {
-		return nil, nil
+		return 0, nil
 	}
 	num, err := strconv.ParseFloat(str, 64)
 	if err != nil {
 		log.Warningm(ctx, "strconv.ParseFloat", err)
-		return nil, err
+		return 0, err
 	}
-	return &num, nil
+	return num, nil
 }
