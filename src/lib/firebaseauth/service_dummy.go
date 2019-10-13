f61b6ae668df8f9791dc6e42f8a719e40a10a0c1
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-10-14 04:52:53 +0900

    refactor: firebase auth lib

--- src/lib/firebaseauth/service_dummy.go
+++ src/lib/firebaseauth/service_dummy.go
@@ -2,25 +2,71 @@ package firebaseauth
 
 import (
 	"context"
-	"net/http"
+	"fmt"
+
+	"github.com/ka-nabellinc/rakutore-backend/appengine/src/lib/log"
 )
 
-type dummyService struct {
-}
-
-// SetCustomClaims ... set custom claims
-func (s *dummyService) SetCustomClaims(ctx context.Context, userID string, claims Claims) error {
-	return nil
+type serviceDebug struct {
 }
 
 // Authentication ... authenticate
-func (s *dummyService) Authentication(ctx context.Context, r *http.Request) (string, Claims, error) {
-	userID := "DUMMY_USER_ID"
-	claims := Claims{}
+func (s *serviceDebug) Authentication(ctx context.Context, ah string) (string, *Claims, error) {
+	var userID string
+	claims := &Claims{}
+
+	// ユーザーを取得できたらデバッグリクエストと判定する
+	if user := getUserByAuthHeader(ah); user != "" {
+		claims = newDummyClaims()
+		return user, claims, nil
+	}
+
+	// 通常の認証を行う
+	token := getTokenByAuthHeader(ah)
+	if token == "" {
+		err := log.Warninge(ctx, "token empty error")
+		return userID, claims, err
+	}
+
+	c, err := getAuthClient(ctx)
+	if err != nil {
+		log.Warningm(ctx, "getAuthClient", err)
+		return userID, claims, err
+	}
+
+	t, err := c.VerifyIDToken(ctx, token)
+	if err != nil {
+		msg := fmt.Sprintf("c.VerifyIDToken: %s", token)
+		log.Warningm(ctx, msg, err)
+		return userID, claims, err
+	}
+
+	userID = t.UID
+	claims.SetMap(t.Claims)
+
 	return userID, claims, nil
 }
 
-// NewDummyService ... get dummy service
-func NewDummyService() Service {
-	return &dummyService{}
+// SetCustomClaims ... set custom claim
+func (s *serviceDebug) SetCustomClaims(ctx context.Context, userID string, claims *Claims) error {
+	c, err := getAuthClient(ctx)
+	if err != nil {
+		log.Errorm(ctx, "getAuthClient", err)
+		return err
+	}
+
+	ah := getAuthHeader(ctx)
+	if getUserByAuthHeader(ah) == "" {
+		err = c.SetCustomUserClaims(ctx, userID, claims.ToMap())
+		if err != nil {
+			log.Errorm(ctx, "c.SetCustomUserClaims", err)
+			return err
+		}
+	}
+	return nil
+}
+
+// NewDebugService ... DebugServiceを作成する
+func NewDebugService() Service {
+	return &serviceDebug{}
 }
