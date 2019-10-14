4bd7738c2f081b52595cecefab1aad81c07f6b23
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-10-14 22:12:35 +0900

    fix: repository

--- src/infrastructure/repository/user_impl.go
+++ src/infrastructure/repository/user_impl.go
@@ -3,53 +3,33 @@ package repository
 import (
 	"context"
 
-	sq "github.com/Masterminds/squirrel"
 	"github.com/abyssparanoia/rapid-go/src/domain/repository"
 	"github.com/abyssparanoia/rapid-go/src/infrastructure/entity"
 	"github.com/abyssparanoia/rapid-go/src/lib/log"
 	"github.com/abyssparanoia/rapid-go/src/lib/mysql"
-	"github.com/jmoiron/sqlx"
 )
 
 type user struct {
-	sql *sqlx.DB
+	cli *mysql.Client
 }
 
 func (r *user) Get(ctx context.Context, userID int64) (*entity.User, error) {
 
-	user := &entity.User{}
+	dsts := []*entity.User{}
 
-	qb := sq.Select("*").From("users").Where(sq.Eq{"id": userID})
+	db := r.cli.GetDB(ctx).
+		Where("id = ?", userID).
+		Find(&dsts)
 
-	mysql.DumpSelectQuery(ctx, qb)
-
-	query, attrs, err := qb.ToSql()
-	if err != nil {
-		log.Errorf(ctx, "sq.Select: %s", err.Error())
+	if err := mysql.HandleErrors(db); err != nil {
+		log.Errorm(ctx, "db.Find", err)
 		return nil, err
 	}
 
-	rows, err := r.sql.QueryxContext(ctx, query, attrs...)
-	if err != nil {
-		log.Errorf(ctx, "r.sql.Queryx: %s", err.Error())
-		return nil, err
-	}
-
-	for rows.Next() {
-		err := rows.StructScan(&user)
-		if err != nil {
-			log.Errorf(ctx, "r.sql.Queryx: %s", err.Error())
-			return nil, err
-		}
-		break
-	}
-
-	return user, nil
+	return dsts[0], nil
 }
 
 // NewUser ... get user repository
-func NewUser(sql *sqlx.DB) repository.User {
-	return &user{
-		sql: sql,
-	}
+func NewUser(cli *mysql.Client) repository.User {
+	return &user{cli}
 }
