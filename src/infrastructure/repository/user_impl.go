5a87cda106b73d0802e1f7123d3a5120dea2a0a1
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-10-14 23:43:59 +0900

    fix: repository

--- src/infrastructure/repository/user_impl.go
+++ src/infrastructure/repository/user_impl.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"context"
 
+	"github.com/abyssparanoia/rapid-go/src/domain/model"
 	"github.com/abyssparanoia/rapid-go/src/domain/repository"
 	"github.com/abyssparanoia/rapid-go/src/infrastructure/entity"
 	"github.com/abyssparanoia/rapid-go/src/lib/log"
@@ -13,12 +14,13 @@ type user struct {
 	cli *mysql.Client
 }
 
-func (r *user) Get(ctx context.Context, userID int64) (*entity.User, error) {
+func (r *user) Get(ctx context.Context, userID string) (*model.User, error) {
 
 	dsts := []*entity.User{}
 
 	db := r.cli.GetDB(ctx).
 		Where("id = ?", userID).
+		Limit(1).
 		Find(&dsts)
 
 	if err := mysql.HandleErrors(db); err != nil {
@@ -26,7 +28,7 @@ func (r *user) Get(ctx context.Context, userID int64) (*entity.User, error) {
 		return nil, err
 	}
 
-	return dsts[0], nil
+	return model.NewUsers(dsts)[0], nil
 }
 
 // NewUser ... get user repository
