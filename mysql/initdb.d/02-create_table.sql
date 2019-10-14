677bee7b9b22231a67238cf071a546516cf851a2
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-10-14 23:16:26 +0900

    fix: create sql

--- mysql/initdb.d/02-create_table.sql
+++ mysql/initdb.d/02-create_table.sql
@@ -1,9 +1,16 @@
-CREATE TABLE `rapid_go`.`users`
-(
-    id int not null primary key,
-	name varchar(255) not null,
-    sex varchar(255) not null,
-    enabled boolean not null,
-	created_at int not null,
-	updated_at int not null
-)
+USE `rapid_go`;
+
+CREATE TABLE `users` (
+  `id` varchar(50) NOT NULL COMMENT 'firebase auth UID',
+  `display_name` varchar(36) NOT NULL COMMENT 'display name',
+  `icon_image_path` varchar(3000) NOT NULL COMMENT 'profile icon image path',
+  `background_image_path` varchar(3000) NOT NULL COMMENT 'background image path',
+  `profile` varchar(1024) NOT NULL COMMENT 'profile text',
+  `email` varchar(1024) NULL COMMENT 'email address',
+  `is_anonymous` boolean NOT NULL DEFAULT TRUE COMMENT 'anonymous or not',
+  `created_at` bigint(20) NOT NULL COMMENT 'creation date',
+  `updated_at` bigint(20) NOT NULL COMMENT 'updation date',
+  `deleted_at` boolean NOT NULL DEFAULT TRUE COMMENT 'delete date'
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+COMMENT "users table";
