30cd0b0d70ac6c31acdfa9ecb834e5a96a3b530d
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-12-09 04:35:46 +0900

    refactor: entity object

--- src/domain/model/user.go
+++ src/domain/model/user.go
@@ -1,7 +1,5 @@
 package model
 
-import "github.com/abyssparanoia/rapid-go/src/infrastructure/entity"
-
 // User ... user model
 type User struct {
 	ID                  string  `json:"id"`
@@ -10,26 +8,6 @@ type User struct {
 	BackgroundImagePath string  `json:"background_image_path"`
 	Profile             *string `json:"profile"`
 	Email               *string `json:"email"`
-}
-
-func newBaseUser(e *entity.User) *User {
-	return &User{
-		ID:                  e.ID,
-		DisplayName:         e.DisplayName,
-		IconImagePath:       e.IconImagePath,
-		BackgroundImagePath: e.BackgroundImagePath,
-		Profile:             e.Profile,
-		Email:               e.Email,
-	}
-}
-
-// NewUsers ... convert from entity to model
-func NewUsers(users []*entity.User) []*User {
-	dsts := []*User{}
-	for _, user := range users {
-		dsts = append(dsts, newBaseUser(user))
-	}
-	return dsts
 }
 
 // IsExist ... check exist or not
--- src/infrastructure/entity/user.go
+++ src/infrastructure/entity/user.go
@@ -1,5 +1,7 @@
 package entity
 
+import "github.com/abyssparanoia/rapid-go/src/domain/model"
+
 // User ... user entity
 type User struct {
 	ID                  string
@@ -9,6 +11,39 @@ type User struct {
 	Profile             *string
 	Email               *string
 	BaseEntity
+}
+
+// BuildFromModel ... build from model
+func (e *User) BuildFromModel(m *model.User) {
+	e.ID = m.ID
+	e.DisplayName = m.DisplayName
+	e.IconImagePath = m.IconImagePath
+	e.BackgroundImagePath = m.BackgroundImagePath
+	e.Profile = m.Profile
+	e.Email = m.Email
+}
+
+// OutputModel ... output model from entity
+func (e *User) OutputModel() *model.User {
+	return &model.User{
+		ID:                  e.ID,
+		DisplayName:         e.DisplayName,
+		IconImagePath:       e.IconImagePath,
+		BackgroundImagePath: e.BackgroundImagePath,
+		Profile:             e.Profile,
+		Email:               e.Email,
+	}
+}
+
+// NewUsers ... output multi user models from entities
+func NewUsers(dsts []*User) []*model.User {
+
+	users := []*model.User{}
+	for _, dst := range dsts {
+		users = append(users, dst.OutputModel())
+	}
+
+	return users
 }
 
 // UserTableName ... get user table name
