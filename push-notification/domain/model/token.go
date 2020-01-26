e4e577d0877eac9359fd93bda9d7eafb793aec85
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-01-27 02:27:07 +0900

    feat: added token model

---
+++ push-notification/domain/model/token.go
@@ -0,0 +1,53 @@
+package model
+
+import (
+	"context"
+
+	"github.com/abyssparanoia/rapid-go/internal/pkg/log"
+	"github.com/abyssparanoia/rapid-go/internal/pkg/util"
+)
+
+// Token ... token model
+type Token struct {
+	ID        string
+	Platform  Platform
+	DeviceID  string
+	Token     string
+	CreatedAt int64
+}
+
+// NewToken ... new token model
+func NewToken(platform Platform,
+	deviceID, token string,
+) *Token {
+	return &Token{
+		Platform:  platform,
+		DeviceID:  deviceID,
+		Token:     token,
+		CreatedAt: util.TimeNowUnixMill(),
+	}
+}
+
+// Platform ... platform type
+type Platform string
+
+const (
+	// PlatformIOS ... ios
+	PlatformIOS Platform = "ios"
+	// PlatformAndroid ... android
+	PlatformAndroid Platform = "android"
+	// PlatformWeb ... web
+	PlatformWeb Platform = "web"
+)
+
+// NewPlatform ... new platform
+func NewPlatform(ctx context.Context, platform string) (Platform, error) {
+
+	switch Platform(platform) {
+	case PlatformIOS, PlatformAndroid, PlatformWeb:
+		return Platform(platform), nil
+	default:
+	}
+
+	return "", log.Errore(ctx, "no match case")
+}
