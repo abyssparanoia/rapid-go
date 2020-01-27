bfeaf8a2564262f3af37df6b044938ad28161d66
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-01-28 01:06:57 +0900

    feat: impl subscribe topic

---
+++ push-notification/infrastructure/repository/fcm_impl.go
@@ -0,0 +1,41 @@
+package repository
+
+import (
+	"context"
+
+	"firebase.google.com/go/messaging"
+	"github.com/abyssparanoia/rapid-go/push-notification/domain/repository"
+
+	"github.com/abyssparanoia/rapid-go/internal/pkg/log"
+)
+
+type fcm struct {
+	messagingClient *messaging.Client
+	serverKey       string
+}
+
+func (r *fcm) SubscribeTopic(
+	ctx context.Context,
+	appID string,
+	topic string,
+	tokens []string) error {
+
+	res, err := r.messagingClient.SubscribeToTopic(ctx, tokens, topic)
+	if err != nil {
+		log.Errorm(ctx, "r.fCli.SubscribeToTopic", err)
+		return err
+	}
+	if res.FailureCount > 0 {
+		for _, rerr := range res.Errors {
+			err = log.Warninge(ctx, "SubscribeToTopic index: %d, reason: %s", rerr.Index, rerr.Reason)
+			return err
+		}
+	}
+	return nil
+}
+
+// NewFcm ... new fcm repository
+func NewFcm(messagingClient *messaging.Client,
+	serverKey string) repository.Fcm {
+	return &fcm{messagingClient, serverKey}
+}
