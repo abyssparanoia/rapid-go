99d72f1dcd9f74fa692462489f83dd75409d5296
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-01-30 20:50:22 +0900

    feat: impl token delete handler

--- push-notification/handler/api/token.go
+++ push-notification/handler/api/token.go
@@ -16,7 +16,7 @@ type TokenHandler struct {
 	tokenUsecase usecase.Token
 }
 
-// Set ... token set
+// Set ... token set handler
 func (h *TokenHandler) Set(w http.ResponseWriter, r *http.Request) {
 
 	ctx := r.Context()
@@ -38,7 +38,7 @@ func (h *TokenHandler) Set(w http.ResponseWriter, r *http.Request) {
 
 	dto, err := input.NewTokenSet(ctx, param.AppID, param.UserID, param.Platform, param.DeviceID, param.Token)
 	if err != nil {
-		renderer.HandleError(ctx, w, "paramater.GetJSON", err)
+		renderer.HandleError(ctx, w, "input.NewTokenSet", err)
 		return
 	}
 
@@ -49,4 +49,43 @@ func (h *TokenHandler) Set(w http.ResponseWriter, r *http.Request) {
 	}
 
 	renderer.Success(ctx, w)
+}
+
+// Delete ... delete token handler
+func (h *TokenHandler) Delete(w http.ResponseWriter, r *http.Request) {
+
+	ctx := r.Context()
+
+	var param struct {
+		AppID    string `json:"app_id" validate:"required"`
+		UserID   string `json:"user_id" validate:"required"`
+		Platform string `json:"platform" validate:"required"`
+		DeviceID string `json:"device_id" validate:"required"`
+	}
+
+	err := parameter.GetJSON(r, &param)
+	if err != nil {
+		err = errcode.Set(err, http.StatusBadRequest)
+		renderer.HandleError(ctx, w, "paramater.GetJSON", err)
+		return
+	}
+
+	dto, err := input.NewTokenDelete(ctx, param.AppID, param.UserID, param.Platform, param.DeviceID)
+	if err != nil {
+		renderer.HandleError(ctx, w, "input.NewTokenDelete", err)
+		return
+	}
+
+	err = h.tokenUsecase.Delete(ctx, dto)
+	if err != nil {
+		renderer.HandleError(ctx, w, "h.tokenUsecase.Delete", err)
+		return
+	}
+
+	renderer.Success(ctx, w)
+}
+
+// NewTokenHandler ... new token handler
+func NewTokenHandler(tokenUsecase usecase.Token) *TokenHandler {
+	return &TokenHandler{tokenUsecase}
 }
