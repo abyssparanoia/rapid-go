0a9b61bdc90e8579dd0a7b4359712442a7feccb3
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-01-30 21:29:23 +0900

    feat: impl send to all user

--- push-notification/handler/api/message.go
+++ push-notification/handler/api/message.go
@@ -85,6 +85,39 @@ func (h *MessageHandler) SendToMultiUser(w http.ResponseWriter, r *http.Request)
 	renderer.Success(ctx, w)
 }
 
+// SendToAllUser ... send to all user handler
+func (h *MessageHandler) SendToAllUser(w http.ResponseWriter, r *http.Request) {
+
+	ctx := r.Context()
+
+	var param struct {
+		AppID          string                `json:"app_id" validate:"required"`
+		MessageRequest *input.MessageRequest `json:"message" validate:"required"`
+	}
+
+	err := parameter.GetJSON(r, &param)
+	if err != nil {
+		err = errcode.Set(err, http.StatusBadRequest)
+		renderer.HandleError(ctx, w, "paramater.GetJSON", err)
+		return
+	}
+
+	v := validator.New()
+	if err := v.Struct(param); err != nil {
+		renderer.HandleError(ctx, w, "validation error: ", err)
+		return
+	}
+
+	dto := input.NewMessageSendToAllUser(param.AppID, param.MessageRequest)
+	err = h.messageUsecase.SendToAllUser(ctx, dto)
+	if err != nil {
+		renderer.HandleError(ctx, w, "h.messageUsecase.SendToAllUser", err)
+		return
+	}
+
+	renderer.Success(ctx, w)
+}
+
 // NewMessageHandler ... new message handler
 func NewMessageHandler(messageUsecase usecase.Message) *MessageHandler {
 	return &MessageHandler{messageUsecase}
