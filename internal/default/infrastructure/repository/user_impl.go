040271e172a8899f71e7a6a39b01b85dfefdfc08
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-04-28 07:55:39 +0900

    feat: impl user create

--- internal/default/infrastructure/repository/user_impl.go
+++ internal/default/infrastructure/repository/user_impl.go
@@ -5,6 +5,8 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/volatiletech/sqlboiler/boil"
+
 	"github.com/abyssparanoia/rapid-go/internal/dbmodels/defaultdb"
 	"github.com/abyssparanoia/rapid-go/internal/default/domain/model"
 	"github.com/abyssparanoia/rapid-go/internal/default/domain/repository"
@@ -37,6 +39,20 @@ func (r *user) Get(ctx context.Context, userID string) (*model.User, error) {
 
 	user := entity.User{User: *dbUser}
 	return user.OutputModel(), nil
+}
+
+func (r *user) Create(
+	ctx context.Context,
+	user *model.User,
+) (*model.User, error) {
+	dbUser := entity.NewUserFromModel(user)
+
+	err := dbUser.Insert(ctx, gluesqlboiler.GetContextExecutor(ctx), boil.Infer())
+	if err != nil {
+		return nil, err
+	}
+
+	return dbUser.OutputModel(), nil
 }
 
 // NewUser ... get user repository
