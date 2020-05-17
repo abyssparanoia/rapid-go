9dd442cb292141b2b2716b1cb09abaa4c7796176
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-05-18 06:14:48 +0900

    feat: added cmd

--- cmd/rapid/root.go
+++ cmd/rapid/root.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 
+	defaultgrpc "github.com/abyssparanoia/rapid-go/cmd/rapid/default-grpc"
+
 	"github.com/spf13/cobra"
 )
 
@@ -18,6 +20,7 @@ func newCmdRoot() *cobra.Command {
 			}
 		},
 	}
+	cmd.AddCommand(defaultgrpc.NewDefaultGRPCCmd())
 	return cmd
 }
 
