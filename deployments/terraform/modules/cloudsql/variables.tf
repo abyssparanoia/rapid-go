409f925c21fe655154f319e8150fd75de36b55b4
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2022-10-09 03:42:59 +0900

    feat: add cloudsql

---
+++ deployments/terraform/modules/cloudsql/variables.tf
@@ -0,0 +1,27 @@
+variable "location" {
+  type = string
+}
+
+variable "tier" {
+  type = string
+}
+
+variable "disk_type" {
+  type = string
+}
+
+variable "availability_type" {
+  type = string
+}
+
+variable "db_instance_name" {
+  type = string
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "db_user" {
+  type = string
+}
