022f5bef153f8e6cbd4ecb97c0d8d98c38b1d8d3
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2022-10-26 10:44:54 +0900

    fix: marshaller

--- internal/domain/model/factory/factory.go
+++ internal/domain/model/factory/factory.go
@@ -1,13 +1,16 @@
 package factory
 
 import (
+	"time"
+
 	"github.com/abyssparanoia/rapid-go/internal/domain/model"
 	"github.com/abyssparanoia/rapid-go/internal/pkg/now"
 	"github.com/bxcodec/faker"
 )
 
 func NewFactory() struct {
-	Tenant *model.Tenant
+	RequestTime time.Time
+	Tenant      *model.Tenant
 } {
 	n := now.Now()
 
@@ -19,7 +22,11 @@ func NewFactory() struct {
 	tenant.CreatedAt = n
 	tenant.UpdatedAt = n
 
-	return struct{ Tenant *model.Tenant }{
-		Tenant: tenant,
+	return struct {
+		RequestTime time.Time
+		Tenant      *model.Tenant
+	}{
+		RequestTime: n,
+		Tenant:      tenant,
 	}
 }
