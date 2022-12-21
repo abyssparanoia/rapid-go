0f7dfbf7b3235830c422f301a181d545dc937ce6
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2022-12-22 02:47:26 +0900

    feat: impl asset service

---
+++ internal/usecase/input/admin_asset.go
@@ -0,0 +1,32 @@
+package input
+
+import (
+	"github.com/abyssparanoia/rapid-go/internal/domain/model"
+	"github.com/abyssparanoia/rapid-go/internal/pkg/errors"
+	"github.com/abyssparanoia/rapid-go/internal/pkg/validation"
+)
+
+type AdminCreateAssetPresignedURL struct {
+	ContentType string          `validate:"required"`
+	AssetType   model.AssetType `validate:"required"`
+}
+
+func NewAdminCreateAssetPresignedURL(
+	contentType string,
+	assetType model.AssetType,
+) *AdminCreateAssetPresignedURL {
+	return &AdminCreateAssetPresignedURL{
+		ContentType: contentType,
+		AssetType:   assetType,
+	}
+}
+
+func (p *AdminCreateAssetPresignedURL) Validate() error {
+	if err := validation.Validate(p); err != nil {
+		return errors.RequestInvalidArgumentErr.Wrap(err)
+	}
+	if !p.AssetType.Valid() {
+		return errors.RequestInvalidArgumentErr.Errorf("invalid asset type %s", p.AssetType)
+	}
+	return nil
+}
