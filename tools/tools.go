a5cf16206b11c645e1fd732c6efafb084dfacac9
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2022-12-23 02:15:43 +0900

    fix: remove tools

--- tools/tools.go
+++ tools/tools.go
@@ -5,7 +5,6 @@ import (
 	_ "github.com/cosmtrek/air"                                      //nolint
 	_ "github.com/golang/mock/mockgen"                               //nolint
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"          //nolint
-	_ "github.com/pressly/goose/v3/cmd/goose"                        //nolint
 	_ "github.com/volatiletech/sqlboiler/v4"                         //nolint
 	_ "github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-mysql" //nolint
 	_ "golang.org/x/tools/cmd/goimports"                             //nolint
