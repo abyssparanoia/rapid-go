3fba20c0eb6354640fa8cde0b289c17ba725c449
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2022-12-23 03:19:34 +0900

    refactor: divide env

--- internal/infrastructure/environment/env.go
+++ internal/infrastructure/environment/env.go
@@ -1,12 +1,16 @@
 package environment
 
+type DatabaseEnvironment struct {
+	DBHost     string `env:"DB_HOST,required"`
+	DBUser     string `env:"DB_USER,required"`
+	DBPassword string `env:"DB_PASSWORD,required"`
+	DBDatabase string `env:"DB_DATABASE,required"`
+}
+
 type Environment struct {
 	Port        string `env:"PORT,required"`
 	Environment string `env:"ENV,required"`
-	DBHost      string `env:"DB_HOST,required"`
-	DBUser      string `env:"DB_USER,required"`
-	DBPassword  string `env:"DB_PASSWORD,required"`
-	DBDatabase  string `env:"DB_DATABASE,required"`
+	DatabaseEnvironment
 
 	GCPProjectID         string `env:"GCP_PROJECT_ID,required"`
 	FirebaseClientAPIKey string `env:"FIREBASE_CLIENT_API_KEY"`
