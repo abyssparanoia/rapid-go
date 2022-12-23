1db4999ca9122302327f0379434be29c1b0c962c
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2022-12-24 03:33:10 +0900

    refactor: env

--- internal/infrastructure/environment/env.go
+++ internal/infrastructure/environment/env.go
@@ -1,5 +1,12 @@
 package environment
 
+type Environment struct {
+	Port        string `env:"PORT,required"`
+	Environment string `env:"ENV,required"`
+	DatabaseEnvironment
+	GCPEnviroment
+}
+
 type DatabaseEnvironment struct {
 	DBHost     string `env:"DB_HOST,required"`
 	DBUser     string `env:"DB_USER,required"`
@@ -7,11 +14,7 @@ type DatabaseEnvironment struct {
 	DBDatabase string `env:"DB_DATABASE,required"`
 }
 
-type Environment struct {
-	Port        string `env:"PORT,required"`
-	Environment string `env:"ENV,required"`
-	DatabaseEnvironment
-
+type GCPEnviroment struct {
 	GCPProjectID         string `env:"GCP_PROJECT_ID,required"`
 	FirebaseClientAPIKey string `env:"FIREBASE_CLIENT_API_KEY"`
 	BucketName           string `env:"BUCKET_NAME,required"`
