d310d272063a4b0159a067292985e869bdd1eed6
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2022-12-24 20:16:39 +0900

    feat: impl aws cognito authentication

--- internal/infrastructure/cognito/internal/dto/claims.go
+++ internal/infrastructure/cognito/internal/dto/claims.go
@@ -1,24 +1,75 @@
 package dto
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/volatiletech/null/v8"
 )
 
 type AWSCognitoClaims struct {
-	ClientID string `json:"client_id"`
-	Username string `json:"username"`
+	Username      string `json:"cognito:username"`
+	Email         string `json:"email"`
+	EmailVerified bool   `json:"email_verified"`
+	TenantID      string `json:"custom:tenant_id,omitepmty"`
+	UserID        string `json:"custom:user_id,omitepmty"`
+	UserRole      string `json:"custom:user_role,omitepmty"`
 	jwt.StandardClaims
 }
 
+const (
+	attributesTenantIDKey = "custom:tenant_id"
+	attributesUserIDKey   = "custom:user_id"
+	attributesUserRoleKey = "custom:user_role"
+)
+
 type UserAttributes struct {
+	AuthUID string
+	CustomUserAttributes
+}
+
+func NewUserAttributesFromCognitoUser(cognitoUser *cognitoidentityprovider.UserType) *UserAttributes {
+	userAttributes := &UserAttributes{
+		AuthUID: *cognitoUser.Username,
+	}
+	for _, attribute := range cognitoUser.Attributes {
+		if *attribute.Name == attributesTenantIDKey {
+			userAttributes.TenantID = null.StringFrom(*attribute.Value)
+		}
+		if *attribute.Name == attributesUserIDKey {
+			userAttributes.UserID = null.StringFrom(*attribute.Value)
+		}
+		if *attribute.Name == attributesUserRoleKey {
+			userAttributes.UserRole = null.StringFrom(*attribute.Value)
+		}
+	}
+
+	return userAttributes
+}
+
+func NewUserAttributesFromClaims(awsClaims *AWSCognitoClaims) *UserAttributes {
+	userAttributes := &UserAttributes{
+		AuthUID: awsClaims.Username,
+	}
+	if awsClaims.TenantID != "" {
+		userAttributes.TenantID = null.StringFrom(awsClaims.TenantID)
+	}
+	if awsClaims.UserID != "" {
+		userAttributes.UserID = null.StringFrom(awsClaims.UserID)
+	}
+	if awsClaims.UserRole != "" {
+		userAttributes.UserRole = null.StringFrom(awsClaims.UserRole)
+	}
+	return userAttributes
+}
+
+type CustomUserAttributes struct {
 	TenantID null.String
 	UserID   null.String
 	UserRole null.String
 }
 
-func (ua *UserAttributes) ToSlice() []*cognitoidentityprovider.AttributeType {
+func (ua *CustomUserAttributes) ToSlice() []*cognitoidentityprovider.AttributeType {
 	attrs := []*cognitoidentityprovider.AttributeType{}
 	if ua.TenantID.Valid {
 		attrs = append(attrs, NewUserAttribute("custom:tenant_id", ua.TenantID.String))
@@ -33,8 +84,9 @@ func (ua *UserAttributes) ToSlice() []*cognitoidentityprovider.AttributeType {
 }
 
 func NewUserAttribute(name, value string) *cognitoidentityprovider.AttributeType {
-	attr := &cognitoidentityprovider.AttributeType{}
-	attr.SetName(name).
-		SetValue(value)
+	attr := &cognitoidentityprovider.AttributeType{
+		Name:  aws.String(name),
+		Value: aws.String(value),
+	}
 	return attr
 }
