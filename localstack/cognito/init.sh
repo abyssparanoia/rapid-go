254b128d5272af529807761c414c5151e6b35f95
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2022-12-24 20:17:06 +0900

    feat: impl aws cognito authentication

--- localstack/cognito/init.sh
+++ localstack/cognito/init.sh
@@ -7,6 +7,17 @@ user_pool_id=$(aws \
         Name="user_id",AttributeDataType="String",DeveloperOnlyAttribute=false,Required=false,StringAttributeConstraints="{MinLength=1,MaxLength=256}" \
         Name="user_role",AttributeDataType="String",DeveloperOnlyAttribute=false,Required=false,StringAttributeConstraints="{MinLength=1,MaxLength=256}" \
     --query UserPool.Id \
+    | sed 's/"//g' \
     )
 
-echo "user pool id is ${user_pool_id}"
+echo "user pool id is $user_pool_id"
+
+client_id=$(aws \
+    --endpoint-url=http://localhost:9229 \
+    cognito-idp create-user-pool-client \
+    --user-pool-id $user_pool_id \
+    --client-name local-client \
+    --query UserPoolClient.ClientId
+    )
+
+echo "client id is ${client_id}"
