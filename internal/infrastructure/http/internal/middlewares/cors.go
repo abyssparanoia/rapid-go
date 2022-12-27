a41959609d944b6c6f48693324bac5d06110b33d
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2022-12-27 22:49:10 +0900

    feat: impl cors middleware

---
+++ internal/infrastructure/http/internal/middlewares/cors.go
@@ -0,0 +1,15 @@
+package middlewares
+
+import "net/http"
+
+func CORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, ResponseType")
+		if r.Method == "OPTIONS" {
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
+}
