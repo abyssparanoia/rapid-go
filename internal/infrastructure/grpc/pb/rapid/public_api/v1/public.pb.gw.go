450a5173b75181e34dde96b0a1affbbfb9c56cc7
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-01-06 00:14:08 +0900

    feat: impl deep health check

--- internal/infrastructure/grpc/pb/rapid/public_api/v1/public.pb.gw.go
+++ internal/infrastructure/grpc/pb/rapid/public_api/v1/public.pb.gw.go
@@ -31,6 +31,24 @@ var _ = runtime.String
 var _ = utilities.NewDoubleArray
 var _ = metadata.Join
 
+func request_PublicV1Service_PublicDeepHealthCheck_0(ctx context.Context, marshaler runtime.Marshaler, client PublicV1ServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq PublicDeepHealthCheckRequest
+	var metadata runtime.ServerMetadata
+
+	msg, err := client.PublicDeepHealthCheck(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_PublicV1Service_PublicDeepHealthCheck_0(ctx context.Context, marshaler runtime.Marshaler, server PublicV1ServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq PublicDeepHealthCheckRequest
+	var metadata runtime.ServerMetadata
+
+	msg, err := server.PublicDeepHealthCheck(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 func request_PublicV1Service_PublicSignIn_0(ctx context.Context, marshaler runtime.Marshaler, client PublicV1ServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq PublicSignInRequest
 	var metadata runtime.ServerMetadata
@@ -123,6 +141,31 @@ func local_request_PublicV1Service_PublicGetTenant_0(ctx context.Context, marsha
 // Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterPublicV1ServiceHandlerFromEndpoint instead.
 func RegisterPublicV1ServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux, server PublicV1ServiceServer) error {
 
+	mux.Handle("GET", pattern_PublicV1Service_PublicDeepHealthCheck_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/rapid.public_api.v1.PublicV1Service/PublicDeepHealthCheck", runtime.WithHTTPPathPattern("/v1/deep_health_check"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_PublicV1Service_PublicDeepHealthCheck_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_PublicV1Service_PublicDeepHealthCheck_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_PublicV1Service_PublicSignIn_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -214,6 +257,28 @@ func RegisterPublicV1ServiceHandler(ctx context.Context, mux *runtime.ServeMux, 
 // "PublicV1ServiceClient" to call the correct interceptors.
 func RegisterPublicV1ServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux, client PublicV1ServiceClient) error {
 
+	mux.Handle("GET", pattern_PublicV1Service_PublicDeepHealthCheck_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/rapid.public_api.v1.PublicV1Service/PublicDeepHealthCheck", runtime.WithHTTPPathPattern("/v1/deep_health_check"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_PublicV1Service_PublicDeepHealthCheck_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_PublicV1Service_PublicDeepHealthCheck_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_PublicV1Service_PublicSignIn_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -262,12 +327,16 @@ func RegisterPublicV1ServiceHandlerClient(ctx context.Context, mux *runtime.Serv
 }
 
 var (
+	pattern_PublicV1Service_PublicDeepHealthCheck_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "deep_health_check"}, ""))
+
 	pattern_PublicV1Service_PublicSignIn_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "sign_in"}, ""))
 
 	pattern_PublicV1Service_PublicGetTenant_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "tenants", "tenant_id"}, ""))
 )
 
 var (
+	forward_PublicV1Service_PublicDeepHealthCheck_0 = runtime.ForwardResponseMessage
+
 	forward_PublicV1Service_PublicSignIn_0 = runtime.ForwardResponseMessage
 
 	forward_PublicV1Service_PublicGetTenant_0 = runtime.ForwardResponseMessage
