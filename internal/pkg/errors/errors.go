aaeb3c17ee1228d36a3364b28e2b3fe8556477f6
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-01-06 05:27:03 +0900

    refactor: error message

--- internal/pkg/errors/errors.go
+++ internal/pkg/errors/errors.go
@@ -1,8 +1,22 @@
 package errors
 
+import "github.com/pkg/errors"
+
 const (
 	InternalErr               InternalError     = "An internal error has occurred2"
 	UnauthorizedErr           UnauthorizedError = "Unauthroized"
 	RequestInvalidArgumentErr BadRequestError   = "Request argument is invalid"
 	NotFoundErr               NotFoundError     = "Not found"
 )
+
+func ExtractPlaneErrMessage(err error) string {
+	switch errors.Cause(err) {
+	case UnauthorizedErr:
+		return UnauthorizedErr.Error()
+	case RequestInvalidArgumentErr:
+		return RequestInvalidArgumentErr.Error()
+	case NotFoundErr:
+		return NotFoundErr.Error()
+	}
+	return InternalErr.Error()
+}
