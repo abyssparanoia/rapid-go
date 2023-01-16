2a7e5c8b6bbd9da1f862a5688d6dd06bd273001d
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-01-16 09:32:20 +0900

    refactor: claims constructor

--- internal/infrastructure/cognito/internal/marshaller/claims.go
+++ internal/infrastructure/cognito/internal/marshaller/claims.go
@@ -3,22 +3,23 @@ package marshaller
 import (
 	"github.com/abyssparanoia/rapid-go/internal/domain/model"
 	"github.com/abyssparanoia/rapid-go/internal/infrastructure/cognito/internal/dto"
+	"github.com/abyssparanoia/rapid-go/internal/pkg/nullable"
 	"github.com/volatiletech/null/v8"
 )
 
 func UserAttributesToModel(
 	userAttribute *dto.UserAttributes,
 ) *model.Claims {
-	claims := model.NewClaims(userAttribute.AuthUID)
-	if userAttribute.TenantID.Valid {
-		claims.SetTenantID(userAttribute.TenantID.String)
-	}
-	if userAttribute.UserID.Valid {
-		claims.SetUserID(userAttribute.UserID.String)
-	}
+	var userRole nullable.Type[model.UserRole]
 	if userAttribute.UserRole.Valid {
-		claims.SetUserRole(model.NewUserRole(userAttribute.UserRole.String))
+		userRole = nullable.TypeFrom(model.NewUserRole(userAttribute.UserRole.String))
 	}
+	claims := model.NewClaims(
+		userAttribute.AuthUID,
+		userAttribute.TenantID,
+		userAttribute.UserID,
+		userRole,
+	)
 	return claims
 }
 
