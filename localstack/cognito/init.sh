a54214361062b0c76f4a25229c9beeafcb83106b
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-03-01 05:51:42 +0900

    feat: rename auth to staff auth

--- localstack/cognito/init.sh
+++ localstack/cognito/init.sh
@@ -4,8 +4,8 @@ user_pool_id=$(aws \
     --endpoint-url=http://localhost:9229 \
     cognito-idp create-user-pool --pool-name local-user-pool \
     --schema Name="tenant_id",AttributeDataType="String",DeveloperOnlyAttribute=false,Required=false,StringAttributeConstraints="{MinLength=1,MaxLength=256}" \
-        Name="user_id",AttributeDataType="String",DeveloperOnlyAttribute=false,Required=false,StringAttributeConstraints="{MinLength=1,MaxLength=256}" \
-        Name="user_role",AttributeDataType="String",DeveloperOnlyAttribute=false,Required=false,StringAttributeConstraints="{MinLength=1,MaxLength=256}" \
+        Name="staff_id",AttributeDataType="String",DeveloperOnlyAttribute=false,Required=false,StringAttributeConstraints="{MinLength=1,MaxLength=256}" \
+        Name="staff_role",AttributeDataType="String",DeveloperOnlyAttribute=false,Required=false,StringAttributeConstraints="{MinLength=1,MaxLength=256}" \
     --query UserPool.Id \
     | sed 's/"//g' \
     )
