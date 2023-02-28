0a7e8ae38829d98098a0e7735a89ddad60932d6a
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-03-01 05:51:25 +0900

    feat: rename auth to staff auth

---
+++ internal/infrastructure/grpc/internal/interceptor/session_interceptor/staff_context.go
@@ -0,0 +1,52 @@
+package session_interceptor
+
+import (
+	"context"
+
+	"github.com/abyssparanoia/rapid-go/internal/domain/model"
+	"github.com/abyssparanoia/rapid-go/internal/pkg/errors"
+	"github.com/abyssparanoia/rapid-go/internal/pkg/nullable"
+	"github.com/volatiletech/null/v8"
+)
+
+type StaffSessionContext struct {
+	AuthUID   string
+	TenantID  null.String
+	StaffID   null.String
+	StaffRole nullable.Type[model.StaffRole]
+}
+
+type staffContextKey struct{}
+
+func newStaffSessionContext(claims *model.StaffClaims) *StaffSessionContext {
+	return &StaffSessionContext{
+		AuthUID:   claims.AuthUID,
+		TenantID:  claims.TenantID,
+		StaffID:   claims.StaffID,
+		StaffRole: claims.StaffRole,
+	}
+}
+
+var (
+	staffSessionContextKey staffContextKey = staffContextKey{}
+)
+
+func SaveStaffSessionContext(
+	ctx context.Context,
+	sessionContext *StaffSessionContext,
+) context.Context {
+	return context.WithValue(ctx, staffSessionContextKey, *sessionContext)
+}
+
+func RequireStaffSessionContext(ctx context.Context) (*StaffSessionContext, error) {
+	sctx, ok := GetStaffSessionContext(ctx)
+	if !ok {
+		return nil, errors.UnauthorizedErr.New()
+	}
+	return sctx, nil
+}
+
+func GetStaffSessionContext(ctx context.Context) (*StaffSessionContext, bool) {
+	sessionContext, ok := ctx.Value(staffSessionContextKey).(StaffSessionContext)
+	return &sessionContext, ok
+}
