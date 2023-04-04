0228a3fe23e2db5a052c79981659be709ce353d7
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-04-04 21:38:23 +0900

    feat: use error code

--- internal/domain/errors/errors.go
+++ internal/domain/errors/errors.go
@@ -3,20 +3,33 @@ package errors
 import "github.com/pkg/errors"
 
 const (
-	InternalErr               InternalError     = "An internal error has occurred"
-	UnauthorizedErr           UnauthorizedError = "Unauthroized"
-	RequestInvalidArgumentErr BadRequestError   = "Request argument is invalid"
-	NotFoundErr               NotFoundError     = "Not found"
+	// common error
+	InternalErr               InternalError     = "E100001"
+	UnauthorizedErr           UnauthorizedError = "E100002"
+	RequestInvalidArgumentErr BadRequestError   = "E100003"
+	NotFoundErr               NotFoundError     = "E100004"
+	ConflictErr               ConflictError     = "E100005"
 )
 
-func ExtractPlaneErrMessage(err error) string {
+var errorMessageMap = map[error]string{
+	// common error
+	InternalErr:               "An internal error has occurred",
+	UnauthorizedErr:           "Unauthroized",
+	RequestInvalidArgumentErr: "Request argument is invalid",
+	NotFoundErr:               "Not found",
+	ConflictErr:               "Already Exist",
+}
+
+func ExtractPlaneErrMessage(err error) (code string, message string) {
 	switch errors.Cause(err) {
 	case UnauthorizedErr:
-		return UnauthorizedErr.Error()
+		return UnauthorizedErr.Error(), errorMessageMap[UnauthorizedErr]
 	case RequestInvalidArgumentErr:
-		return RequestInvalidArgumentErr.Error()
+		return RequestInvalidArgumentErr.Error(), errorMessageMap[RequestInvalidArgumentErr]
 	case NotFoundErr:
-		return NotFoundErr.Error()
+		return NotFoundErr.Error(), errorMessageMap[NotFoundErr]
+	case ConflictErr:
+		return ConflictErr.Error(), errorMessageMap[ConflictErr]
 	}
-	return InternalErr.Error()
+	return InternalErr.Error(), errorMessageMap[InternalErr]
 }
