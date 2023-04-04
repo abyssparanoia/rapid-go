478281f3f90d1c8c3ef2196871aef1ae6a840b6a
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-04-04 21:34:55 +0900

    feat: move error dir

---
+++ internal/domain/errors/intenal_error.go
@@ -0,0 +1,49 @@
+package errors
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+// InternalError ... base error
+type InternalError string
+
+func (e InternalError) Error() string {
+	return string(e)
+}
+
+// New ... new error
+func (e InternalError) New() error {
+	return errors.Wrap(e, "")
+}
+
+// Errorf ... errorf
+func (e InternalError) Errorf(format string, args ...interface{}) error {
+	return errors.Wrapf(e, format, args...)
+}
+
+// Wrap ... wrap error
+func (e InternalError) Wrap(err error) error {
+	if err == nil {
+		return e.New()
+	}
+	return errors.Wrap(e, err.Error())
+}
+
+// Wrapf ... wrapf
+func (e InternalError) Wrapf(err error, format string, args ...interface{}) error {
+	if err == nil {
+		return e.Errorf(format, args...)
+	}
+	msg := fmt.Sprintf(format, args...)
+	return errors.Wrapf(e, "err: %s; %s", err, msg)
+}
+
+// As ... as method
+func (e InternalError) As(target interface{}) bool {
+	if _, ok := target.(**InternalError); ok {
+		return true
+	}
+	return false
+}
