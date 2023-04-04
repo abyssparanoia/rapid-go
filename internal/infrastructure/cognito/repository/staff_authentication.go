826ee4d2fe5717d628f311297ad0c2e7079c2481
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-04-04 21:35:03 +0900

    feat: move error dir

--- internal/infrastructure/cognito/repository/staff_authentication.go
+++ internal/infrastructure/cognito/repository/staff_authentication.go
@@ -5,11 +5,11 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/abyssparanoia/rapid-go/internal/domain/errors"
 	"github.com/abyssparanoia/rapid-go/internal/domain/model"
 	"github.com/abyssparanoia/rapid-go/internal/domain/repository"
 	"github.com/abyssparanoia/rapid-go/internal/infrastructure/cognito/internal/dto"
 	"github.com/abyssparanoia/rapid-go/internal/infrastructure/cognito/internal/marshaller"
-	"github.com/abyssparanoia/rapid-go/internal/pkg/errors"
 	"github.com/abyssparanoia/rapid-go/internal/pkg/uuid"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
