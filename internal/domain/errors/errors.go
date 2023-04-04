dcedd2ecaf6e894148d03b34a81c5a95deb1899a
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-04-04 21:50:57 +0900

    feat: add required staff session error

--- internal/domain/errors/errors.go
+++ internal/domain/errors/errors.go
@@ -5,11 +5,10 @@ import "github.com/pkg/errors"
 const (
 	// common error
 	InternalErr                InternalError     = "E100001"
-	UnauthorizedErr            UnauthorizedError = "E100002"
-	RequestInvalidArgumentErr  BadRequestError   = "E100003"
-	NotFoundErr                NotFoundError     = "E100004"
-	ConflictErr                ConflictError     = "E100005"
-	InvalidAdminRequestUserErr UnauthorizedError = "E100006"
+	RequestInvalidArgumentErr  BadRequestError   = "E100002"
+	InvalidIDTokenErr          UnauthorizedError = "E100003"
+	RequireStaffSessionErr     UnauthorizedError = "E100004"
+	InvalidAdminRequestUserErr UnauthorizedError = "E100005"
 
 	// tenant error
 	TenantNotFoundErr NotFoundError = "E200101"
@@ -21,10 +20,9 @@ const (
 var errorMessageMap = map[error]string{
 	// common error
 	InternalErr:                "An internal error has occurred",
-	UnauthorizedErr:            "Unauthroized",
 	RequestInvalidArgumentErr:  "Request argument is invalid",
-	NotFoundErr:                "Not found",
-	ConflictErr:                "Already Exist",
+	InvalidIDTokenErr:          "Invalid ID token",
+	RequireStaffSessionErr:     "Require staff session",
 	InvalidAdminRequestUserErr: "Invalid admin request user",
 
 	// tenant error
@@ -46,16 +44,14 @@ func ExtractPlaneErrMessage(err error) (code string, message string) {
 		return StaffNotFoundErr.Error(), errorMessageMap[StaffNotFoundErr]
 
 	// common error
+	case InvalidIDTokenErr:
+		return InvalidIDTokenErr.Error(), errorMessageMap[InvalidIDTokenErr]
+	case RequireStaffSessionErr:
+		return RequireStaffSessionErr.Error(), errorMessageMap[RequireStaffSessionErr]
 	case InvalidAdminRequestUserErr:
 		return InvalidAdminRequestUserErr.Error(), errorMessageMap[InvalidAdminRequestUserErr]
-	case UnauthorizedErr:
-		return UnauthorizedErr.Error(), errorMessageMap[UnauthorizedErr]
 	case RequestInvalidArgumentErr:
 		return RequestInvalidArgumentErr.Error(), errorMessageMap[RequestInvalidArgumentErr]
-	case NotFoundErr:
-		return NotFoundErr.Error(), errorMessageMap[NotFoundErr]
-	case ConflictErr:
-		return ConflictErr.Error(), errorMessageMap[ConflictErr]
 	}
 	return InternalErr.Error(), errorMessageMap[InternalErr]
 }
