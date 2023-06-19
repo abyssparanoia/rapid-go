e8d673ec1e471c161fa1267b7f3a5660e4251007
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-06-19 20:27:06 +0900

    fix: golangci-lint

--- internal/infrastructure/http/internal/middlewares/cors.go
+++ internal/infrastructure/http/internal/middlewares/cors.go
@@ -7,7 +7,7 @@ func CORS(h http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, ResponseType")
-		if r.Method == "OPTIONS" {
+		if r.Method == http.MethodOptions {
 			return
 		}
 		h.ServeHTTP(w, r)
