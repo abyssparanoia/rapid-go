db06f434775a886bc47c7f0354bcf1a554cb4689
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-08-04 05:08:12 +0900

    feat: impl insert ddl method

--- internal/infrastructure/spanner/internal/dbmodel/yo_db.go
+++ internal/infrastructure/spanner/internal/dbmodel/yo_db.go
@@ -9,6 +9,7 @@ import (
 
 	"cloud.google.com/go/spanner"
 	"github.com/googleapis/gax-go/v2/apierror"
+	"google.golang.org/api/iterator"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -77,3 +78,145 @@ func (e yoError) GRPCStatus() *status.Status {
 func (e yoError) Timeout() bool   { return e.code == codes.DeadlineExceeded }
 func (e yoError) Temporary() bool { return e.code == codes.DeadlineExceeded }
 func (e yoError) NotFound() bool  { return e.code == codes.NotFound }
+
+var (
+	ctxTKey = struct{}{}
+)
+
+func GetSpannerTransaction(ctx context.Context) *SpannerTransaction {
+	if txn, ok := ctx.Value(&ctxTKey).(*SpannerTransaction); ok {
+		return txn
+	}
+	panic("error GetSpannerTransaction: transaction not found")
+}
+
+type SpannerTransactable struct {
+	db *spanner.Client
+}
+
+func (t *SpannerTransactable) BeginRoTransaction(ctx context.Context, fn func(ctx context.Context) error) error {
+
+	err := (func() error {
+		ro := t.db.ReadOnlyTransaction()
+		defer func() { ro.Close() }()
+		txn := &SpannerTransaction{ro: ro}
+		ctxWithTx := context.WithValue(ctx, &ctxTKey, txn)
+		if err := fn(ctxWithTx); err != nil {
+			return err
+		}
+		return nil
+	})()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *SpannerTransactable) BeginRwTransaction(ctx context.Context, fn func(ctx context.Context) error) error {
+	_, err := t.db.ReadWriteTransaction(ctx, func(ctx context.Context, rw *spanner.ReadWriteTransaction) error {
+
+		txn := &SpannerTransaction{rw: rw}
+		ctxWithTx := context.WithValue(ctx, &ctxTKey, txn)
+		// トランザクション内で処理を実行
+		if err := fn(ctxWithTx); err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewTransactable(
+	db *spanner.Client,
+) *SpannerTransactable {
+	return &SpannerTransactable{db}
+}
+
+type SpannerTransaction struct {
+	ro *spanner.ReadOnlyTransaction
+	rw *spanner.ReadWriteTransaction
+}
+
+// isRo はReadOnlyTransactionかどうかを返す
+func (t *SpannerTransaction) isRo() bool {
+	return t.ro != nil
+}
+
+// isRw はReadWriteTransactionかどうかを返す
+func (t *SpannerTransaction) isRw() bool {
+	return t.rw != nil
+}
+
+func (t *SpannerTransaction) QueryContext(ctx context.Context, query string, params map[string]interface{}) (*SpannerRows, error) {
+	if t.isRo() {
+		iter := t.ro.Query(ctx, spanner.Statement{
+			SQL:    query,
+			Params: params,
+		})
+		return &SpannerRows{iter: iter, query: query}, nil
+	}
+	if t.isRw() {
+		iter := t.rw.Query(ctx, spanner.Statement{
+			SQL:    query,
+			Params: params,
+		})
+		return &SpannerRows{iter: iter, query: query}, nil
+	}
+	return nil, fmt.Errorf("error QueryContext: empty transaction")
+}
+
+func (t *SpannerTransaction) ExecContext(ctx context.Context, query string, params map[string]interface{}) error {
+	if t.isRo() || !t.isRw() {
+		return fmt.Errorf("error ExecContext is executed in read only transaction")
+	}
+
+	_, err := t.rw.Update(ctx, spanner.Statement{
+		SQL:    query,
+		Params: params,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type SpannerRows struct {
+	iter    *spanner.RowIterator
+	query   string
+	nextRow *spanner.Row
+}
+
+func (r *SpannerRows) Close() error {
+	r.iter.Stop()
+	return nil
+}
+
+func (r *SpannerRows) Next() (ok bool, err error) {
+	row, err := r.iter.Next()
+	if err == iterator.Done {
+		return false, nil
+	}
+	if err != nil {
+		return false, err
+	}
+	r.nextRow = row
+	return true, nil
+}
+
+func (r *SpannerRows) Scan(ptrs ...interface{}) error {
+	if r.nextRow == nil {
+		return fmt.Errorf("SpannerRows.Scan(): next row is nil")
+	}
+	return r.nextRow.Columns(ptrs...)
+}
+
+func (r *SpannerRows) ToStruct(p interface{}) error {
+	return r.nextRow.ToStruct(p)
+}
