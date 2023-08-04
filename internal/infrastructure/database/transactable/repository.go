6606e73684ef6c00f6f7a6632baca7e97514d8cc
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-08-04 09:38:37 +0900

    feat: impl spanner repository

--- internal/infrastructure/database/transactable/repository.go
+++ internal/infrastructure/database/transactable/repository.go
@@ -12,6 +12,10 @@ func NewTransactable() repository.Transactable {
 	return &transactable{}
 }
 
+func (r *transactable) ROTx(ctx context.Context, fn func(ctx context.Context) error) error {
+	panic("not implement for mysql")
+}
+
 func (r *transactable) RWTx(ctx context.Context, fn func(ctx context.Context) error) error {
 	return RunTx(ctx, fn)
 }
