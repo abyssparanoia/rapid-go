1e5957b94ae571643d9f8eafaf667712896e0bea
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-12-23 19:59:56 +0200

    fix: path

--- internal/infrastructure/redis/helper/transaction.go
+++ internal/infrastructure/redis/helper/transaction.go
@@ -4,7 +4,7 @@ import (
 	"context"
 
 	"github.com/cenkalti/backoff"
-	"github.com/go-redis/redis/v8"
+	"github.com/redis/go-redis/v9"
 )
 
 func RunTransaction(ctx context.Context, redisClient *redis.Client, fn func(ctx context.Context, tx *redis.Tx) error, keys ...string) error {
