baa4b1559629f5c4cc2a284830f750bd6b567f98
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2024-01-29 20:21:09 +0200

    feat: replace gomockgen to uber

--- tools/tools.go
+++ tools/tools.go
@@ -6,11 +6,11 @@ import (
 	_ "github.com/bufbuild/buf/cmd/buf"                              //nolint
 	_ "github.com/cloudspannerecosystem/wrench"                      //nolint
 	_ "github.com/cosmtrek/air"                                      //nolint
-	_ "github.com/golang/mock/mockgen"                               //nolint
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"          //nolint
 	_ "github.com/kauche/splanter"                                   //nolint
 	_ "github.com/volatiletech/sqlboiler/v4"                         //nolint
 	_ "github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-mysql" //nolint
 	_ "go.mercari.io/yo"                                             //nolint
+	_ "go.uber.org/mock/mockgen"                                     //nolint
 	_ "golang.org/x/tools/cmd/goimports"                             //nolint
 )
