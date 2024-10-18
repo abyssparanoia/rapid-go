e819ce2622f24bda0d7ec649b80b3c2c1f386389
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2024-10-18 09:41:31 +0900

    feat: impl database cache asset

--- internal/domain/service/asset_impl.go
+++ internal/domain/service/asset_impl.go
@@ -28,10 +28,12 @@ func (s *assetService) CreatePresignedURL(
 	ctx context.Context,
 	assetType model.AssetType,
 	contentType string,
+	requestTime time.Time,
 ) (*AssetCreatePresignedURLResult, error) {
 	asset, err := model.NewAsset(
 		assetType,
 		contentType,
+		requestTime,
 	)
 	if err != nil {
 		return nil, err
@@ -40,8 +42,7 @@ func (s *assetService) CreatePresignedURL(
 		ctx,
 		contentType,
 		asset.Path,
-		// 有効期限は15分とする
-		15*time.Minute,
+		asset.Expiration(),
 	)
 	if err != nil {
 		return nil, err
@@ -49,14 +50,12 @@ func (s *assetService) CreatePresignedURL(
 	if err := s.assetPathCache.Set(
 		ctx,
 		asset,
-		// 有効期限は30分とする
-		24*time.Hour,
 	); err != nil {
 		return nil, err
 	}
 
 	return &AssetCreatePresignedURLResult{
-		AssetKey:     asset.Key,
+		AssetKey:     asset.ID,
 		PresignedURL: presignedURL,
 	}, nil
 }
