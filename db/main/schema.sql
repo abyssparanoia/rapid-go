8ea0a971bb98235ba45c895f6783e6c96024393d
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2024-10-18 09:41:14 +0900

    feat: impl database cache asset

--- db/main/schema.sql
+++ db/main/schema.sql
@@ -1,10 +1,27 @@
+CREATE TABLE `asset_types` (
+  `id` varchar(256) NOT NULL COMMENT 'id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='asset_type';
+
+CREATE TABLE `assets` (
+  `id` varchar(64) NOT NULL COMMENT 'id',
+  `content_type` varchar(1024) NOT NULL COMMENT 'content_type',
+  `type` varchar(256) NOT NULL COMMENT 'type',
+  `path` text NOT NULL COMMENT 'path',
+  `expires_at` datetime NOT NULL COMMENT 'expires_at',
+  `created_at` datetime NOT NULL COMMENT 'created date',
+  `updated_at` datetime NOT NULL COMMENT 'update date',
+  PRIMARY KEY (`id`),
+  KEY `assets_fkey_type` (`type`),
+  CONSTRAINT `assets_fkey_type` FOREIGN KEY (`type`) REFERENCES `asset_types` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='asset';
+
 CREATE TABLE `goose_db_version` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `version_id` bigint NOT NULL,
   `is_applied` tinyint(1) NOT NULL,
   `tstamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 
 CREATE TABLE `staff_roles` (
@@ -18,10 +35,10 @@ CREATE TABLE `staffs` (
   `role` varchar(32) NOT NULL COMMENT 'role',
   `auth_uid` varchar(256) NOT NULL COMMENT 'auth_uid',
   `display_name` varchar(256) NOT NULL COMMENT 'display_name',
-  `image_path` varchar(1024) NOT NULL COMMENT 'auth_uid',
+  `image_path` varchar(1024) NOT NULL COMMENT 'image_path',
   `email` varchar(512) NOT NULL COMMENT 'email',
-  `created_at` timestamp NOT NULL COMMENT 'created date',
-  `updated_at` timestamp NOT NULL COMMENT 'update date',
+  `created_at` datetime NOT NULL COMMENT 'created date',
+  `updated_at` datetime NOT NULL COMMENT 'update date',
   PRIMARY KEY (`id`),
   UNIQUE KEY `staffs_unique_email` (`email`),
   UNIQUE KEY `staffs_unique_auth_uid` (`auth_uid`),
@@ -34,8 +51,8 @@ CREATE TABLE `staffs` (
 CREATE TABLE `tenants` (
   `id` varchar(64) NOT NULL COMMENT 'id',
   `name` varchar(256) NOT NULL COMMENT 'name',
-  `created_at` timestamp NOT NULL COMMENT 'created date',
-  `updated_at` timestamp NOT NULL COMMENT 'update date',
+  `created_at` datetime NOT NULL COMMENT 'created date',
+  `updated_at` datetime NOT NULL COMMENT 'update date',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='tenant';
 
