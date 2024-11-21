a3fec4818d6e83e6c7a23eec3e68c64953c83942
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2024-11-21 15:00:41 +0900

    feat: impl structured error

---
+++ internal/pkg/logger/logger_field/error.go
@@ -0,0 +1,14 @@
+package logger_field
+
+import (
+	"github.com/abyssparanoia/goerr"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func Error(err error) zapcore.Field {
+	if goErr := goerr.Unwrap(err); goErr != nil {
+		return zap.Object("error", goErr)
+	}
+	return zap.Error(err)
+}
