74bf17382d494fc16a01f9618605c114030009fb
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2025-01-04 15:07:59 +0900

    feat: impl database sync constants cmd

--- internal/infrastructure/database/migration/run.go
+++ internal/infrastructure/database/migration/run.go
@@ -1,13 +1,17 @@
 package migration
 
 import (
+	"context"
 	"os"
 	"path/filepath"
+	"strings"
 
+	constant_files "github.com/abyssparanoia/rapid-go/db/main/constants"
 	migration_files "github.com/abyssparanoia/rapid-go/db/main/migrations"
 	"github.com/abyssparanoia/rapid-go/internal/infrastructure/database"
 	"github.com/abyssparanoia/rapid-go/internal/infrastructure/environment"
 	"github.com/abyssparanoia/rapid-go/internal/pkg/logger"
+	"github.com/abyssparanoia/rapid-go/internal/pkg/logger/logger_field"
 	"github.com/caarlos0/env/v11"
 	"github.com/pressly/goose/v3"
 )
@@ -60,7 +64,7 @@ func RunExtractSchema() {
 	databaseCli := database.NewClient(e.DBHost, e.DBUser, e.DBPassword, e.DBDatabase, true)
 
 	//nolint:execinquery
-	tables, err := databaseCli.DB.Query("SHOW TABLES")
+	tables, err := databaseCli.DB.Query("SHOW TABLES") //nolint:rowserrcheck
 	if err != nil {
 		panic(err)
 	}
@@ -85,6 +89,53 @@ func RunExtractSchema() {
 		_, err := file.WriteString(createStatement + ";\n\n")
 		if err != nil {
 			panic(err)
+		}
+	}
+}
+
+func RunSyncConstants() {
+	e := &environment.DatabaseEnvironment{}
+	if err := env.Parse(e); err != nil {
+		panic(err)
+	}
+	ctx := context.Background()
+
+	l := logger.New()
+	ctx = logger.ToContext(ctx, l)
+
+	logger.L(ctx).Info("start sync constants")
+
+	databaseCli := database.NewClient(e.DBHost, e.DBUser, e.DBPassword, e.DBDatabase, true)
+
+	dirEntries, err := constant_files.EmbedConstants.ReadDir(".")
+	if err != nil {
+		panic(err)
+	}
+
+	tx, err := databaseCli.DB.BeginTx(ctx, nil)
+	if err != nil {
+		panic(err)
+	}
+	var syncErr error
+	defer func() {
+		// If there's an error, rollback the transaction, else commit it
+		if syncErr != nil {
+			if rollbackErr := tx.Rollback(); rollbackErr != nil {
+				logger.L(ctx).Error("Failed to rollback transaction", logger_field.Error(rollbackErr))
+			}
+		} else {
+			if commitErr := tx.Commit(); commitErr != nil {
+				logger.L(ctx).Error("Failed to commit transaction", logger_field.Error(commitErr))
+			}
+		}
+	}()
+
+	for _, entry := range dirEntries {
+		if strings.HasSuffix(entry.Name(), ".yaml") || strings.HasSuffix(entry.Name(), ".yml") {
+			syncErr = syncConstantByYaml(ctx, tx, entry)
+			if syncErr != nil {
+				return
+			}
 		}
 	}
 }
